eaf3169da62d3967fefff70ec7c849c0a0f6d3de
sudhansu koirala <67737425+sudhansu15@users.noreply.github.com>
2021-07-25 13:20:55 +0545

    added a reliable email

--- stonks/settings.py
+++ stonks/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sk260501@gmail.com'
-EMAIL_HOST_PASSWORD = 'peanutbutter@123'
+EMAIL_HOST_USER = 'stonks.care@gmail.com'
+EMAIL_HOST_PASSWORD = ",aa7h_VM&RAV6a%a"
